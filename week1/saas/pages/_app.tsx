8bfc4736b695cf6fee745f9270a716f030f030f7
jexpop <46821108+jexpop@users.noreply.github.com>
2026-02-23 18:33:34 +0100

    clean

--- week1/saas/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';  // Importa los estilos de Tailwind
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
